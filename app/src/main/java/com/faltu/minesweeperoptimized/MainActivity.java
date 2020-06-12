65c731f0db6380576ba81f213791167242e3efbf
Mihir3101 <mihirmaheshwari3101@gmail.com>
2020-06-12 18:33:09 +0530

    initial MineSweeper Game version 1.0

--- app/src/main/java/com/faltu/minesweeperoptimized/MainActivity.java
+++ app/src/main/java/com/faltu/minesweeperoptimized/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -25,21 +27,50 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    int n=10,m=5;
+    int n=12,m=8;
     Button[][] buttonArray=new Button[n][m];
     int[][] matrix= new int[n][m];
-    int noOfBomb=5;
+    int noOfBomb=n;
+    int[] Bombi=new int[noOfBomb];
+    int[] Bombj=new int[noOfBomb];
     Button reset;
     TextView score;
     int score_count=0;
+    Boolean flagButtonClicked=false;
+    ImageButton addFlag;
+    int flagClickNumber=-1;
+    int safeClickCount=0,safeFlagCount=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         reset=(Button)findViewById(R.id.reset);
         score=(TextView)findViewById(R.id.score);
+        addFlag=(ImageButton)findViewById(R.id.addFlag);
+
         createMatrix();
         addButtons();
+        showScore();
+        addFlag.setBackgroundResource(R.drawable.ic_bomb);
+
+        addFlag.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View v) {
+                if(flagClickNumber==0||flagClickNumber==-1) {
+                    flagButtonClicked=true;
+                    flagClickNumber=1;
+                    addFlag.setBackgroundResource(R.drawable.ic_flag_bomb);
+                    //addFlag.setBackgroundColor(Color.MAGENTA);
+                }else{
+                    flagButtonClicked=false;
+                    addFlag.setBackgroundResource(R.drawable.ic_bomb);
+                    //addFlag.setBackgroundColor(0xFF03DAC5);
+                    flagClickNumber=0;
+                }
+            }
+        });
 
         for(int i=0;i<n;i++)
         {
@@ -47,23 +78,67 @@ public class MainActivity extends AppCompatActivity {
             {
                 final int finalI = i;
                 final int finalJ = j;
+
+                /*buttonArray[i][j].setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        buttonArray[finalI][finalJ].setBackgroundResource(R.drawable.ic_flag_bomb);
+                        if(matrix[finalI][finalJ]==-1)
+                        {
+                            score_count+=2;
+                            showScore();
+                            buttonArray[finalI][finalJ].setEnabled(false);
+                        }
+                        flagButtonClicked=true;
+                        return false;
+                    }
+                });*/
+
                 buttonArray[i][j].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        if(matrix[finalI][finalJ]!=-1)
-                        {
-                            buttonArray[finalI][finalJ].setText(Integer.toString(matrix[finalI][finalJ]));
-                            buttonArray[finalI][finalJ].setEnabled(false);
-                            buttonArray[finalI][finalJ].setBackgroundColor(Color.BLUE);
-                            buttonArray[finalI][finalJ].setTextColor(Color.YELLOW);
-                            score_count++;
-                            showScore();
+                        if(flagButtonClicked){
+                            buttonArray[finalI][finalJ].setText("");
+                            buttonArray[finalI][finalJ].setBackgroundResource(R.drawable.ic_flag_bomb);
+                            if(matrix[finalI][finalJ]==-1){
+                                safeFlagCount++;
+                                System.out.println(safeFlagCount);
+                            }
                         }
-                        else{
-                            buttonArray[finalI][finalJ].setBackgroundColor(Color.RED);
-                            AlertDialog.Builder alertDialogBuilder= new AlertDialog.Builder(MainActivity.this);
-                            alertDialogBuilder.setTitle("You Lost the game /*_*/\nYour Score is"+Integer.toString(score_count));
-                            alertDialogBuilder.setIcon(R.drawable.ic_launcher_foreground);
+                        else {
+                            if (matrix[finalI][finalJ] != -1) {
+                                buttonArray[finalI][finalJ].setBackgroundColor(Color.BLUE);
+                                buttonArray[finalI][finalJ].setTextColor(Color.YELLOW);
+                                buttonArray[finalI][finalJ].setText(Integer.toString(matrix[finalI][finalJ]));
+                                buttonArray[finalI][finalJ].setEnabled(false);
+                                score_count++;
+                                showScore();
+                                safeClickCount++;
+                            }
+                        }
+                        Boolean callDialogBox=false;
+                        String RESULT_IN_DIALODUE = null;
+                        int idForIcon=R.id.reset;
+                        if(safeFlagCount==noOfBomb&&safeClickCount==((n*m)-noOfBomb))
+                            {
+                                RESULT_IN_DIALODUE="You Won The Game with Score : "+Integer.toString(score_count);
+                                Toast.makeText(MainActivity.this,"Winnner!!!",Toast.LENGTH_SHORT);
+                                callDialogBox=true;
+                            }
+                        if(matrix[finalI][finalJ]==-1&&!flagButtonClicked){
+                                buttonArray[finalI][finalJ].setText("");
+                                for(int x=0;x<noOfBomb;x++) {
+                                    buttonArray[Bombi[x]][Bombj[x]].setBackgroundColor(Color.RED);
+                                    buttonArray[Bombi[x]][Bombj[x]].setBackgroundResource(R.drawable.ic_bomb);
+                                }
+                                RESULT_IN_DIALODUE="You Lost the Game\n Score : "+Integer.toString(score_count);
+                                callDialogBox=true;
+                                Toast.makeText(MainActivity.this,"You Lost -_-",Toast.LENGTH_SHORT);
+                        }
+                        if(callDialogBox) {
+                            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                            alertDialogBuilder.setTitle(RESULT_IN_DIALODUE);
+                            alertDialogBuilder.setIcon(R.drawable.ic_restart);
                             alertDialogBuilder.setMessage("Do you want to play again!!");
                             alertDialogBuilder.setCancelable(false);
                             alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
@@ -75,12 +150,12 @@ public class MainActivity extends AppCompatActivity {
                             alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
-                                    Toast.makeText(MainActivity.this, "You have clicked Exit", Toast.LENGTH_SHORT).show();
-                                    finish();
+                                    setButtonsDisable();
                                 }
                             });
                             alertDialogBuilder.create().show();
                         }
+
                     }
                 });
             }
@@ -95,20 +170,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void showScore() {
-        score.setText("Score : "+Integer.toString(score_count));
+    private void setButtonsDisable() {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                buttonArray[i][j].setEnabled(false);
+            }
+        }
     }
 
+    private void showScore() {
+        score.setText("  Score : "+Integer.toString(score_count));
+    }
+
+    @SuppressLint("ResourceAsColor")
     private void resetGame() {
         createMatrix();
-        score.setText("Score : 0");
+        safeClickCount=0;
+        safeFlagCount=0;
+        addFlag.setBackgroundResource(R.drawable.ic_bomb);
+        score.setText("  Score : 0");
         score_count=0;
+        flagButtonClicked=false;
+        flagClickNumber=-1;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 buttonArray[i][j].setEnabled(true);
-                buttonArray[i][j].setText(Integer.toString(i)+""+Integer.toString(j));
+                buttonArray[i][j].setText("*");
                 buttonArray[i][j].setBackgroundColor(Color.LTGRAY);
                 buttonArray[i][j].setTextColor(Color.BLACK);
             }
@@ -118,16 +209,20 @@ public class MainActivity extends AppCompatActivity {
     private void addButtons() {
 
         GridLayout layout = (GridLayout) findViewById(R.id.rootLayout);
-        RelativeLayout.LayoutParams rel_bottone = new RelativeLayout.LayoutParams(200, ViewGroup.LayoutParams.MATCH_PARENT);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(120,ViewGroup.LayoutParams.WRAP_CONTENT);
+        int marginValue=2;
+        params.bottomMargin=marginValue;
+        params.topMargin=marginValue;
+        params.leftMargin=marginValue;
+        params.rightMargin=marginValue;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-
                 buttonArray[i][j]=new Button(this);
-                buttonArray[i][j].setText(Integer.toString(i)+""+Integer.toString(j));
+                buttonArray[i][j].setText("*");//Integer.toString(i)+""+Integer.toString(j)
                 buttonArray[i][j].setBackgroundColor(Color.LTGRAY);
-                buttonArray[i][j].setLayoutParams(rel_bottone);
+                buttonArray[i][j].setLayoutParams(params);
                 layout.addView(buttonArray[i][j]);
             }
         }
@@ -149,50 +244,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }*/
 
-
-        /*lin_btn.setWeightSum(3f);
-        for (int j = 0; j < 3; j++) {
-            LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            params1.setMargins(10, 0, 0, 10);
-            params1.weight = 1.0f;
-
-            LinearLayout ll;
-            ll = new LinearLayout(this);
-            ll.setGravity(Gravity.CENTER_VERTICAL);
-            ll.setOrientation(LinearLayout.HORIZONTAL);
-            ll.setLayoutParams(params1);
-
-            final Button btn;
-            btn = new Button(MainActivity.this);
-
-            btn.setText("A"+(j+1));
-            btn.setTextSize(15);
-            btn.setId(j);
-            btn.setPadding(10, 8, 10, 10);
-
-            ll.addView(btn);
-
-            lin_btn.addView(ll);
-
-
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    if(v.getId()==0)
-                    {
-                        txt_text.setText("Hii");
-                    }else if(v.getId()==1)
-                    {
-                        txt_text.setText("hello");
-                    }else if(v.getId()==2)
-                    {
-                        txt_text.setText("how r u");
-                    }
-                }
-            });
-        }*/
     }
 
     private void LostGame() {
@@ -201,13 +252,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void createMatrix() {
         //initially matrix is -51
-        //and Bomb is where matrix[i][j]=-1
+        //and Bomb is where matrix[i][j]=-51
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
-            {
-                matrix[i][j]=-51;
-            }
+            Arrays.fill(matrix[i], -51);
         }
         //Bomb is placed in matrix where matrix[i][j]=-1 && for matrix[i][j]=-51 it is empty
         placeBombs();
@@ -246,8 +294,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }*/
         //Code for Locating Bomb;
-        int[] Bombi=new int[noOfBomb];
-        int[] Bombj=new int[noOfBomb];
         Arrays.fill(Bombi, -1);
         /*for(int i=0;i<noOfBomb;i++)
         {
@@ -285,9 +331,9 @@ public class MainActivity extends AppCompatActivity {
     private int noOfBombsAround(int i, int j) {
         int[] box={0,1,-1};
         int count=0;
-        for(int x=0;x<3;x++)
+        for(int x=0;x<box.length;x++)
         {
-            for(int y=0;y<3;y++)
+            for(int y=0;y<box.length;y++)
             {
                 if(box[x]==0&&box[y]==0)
                 {
